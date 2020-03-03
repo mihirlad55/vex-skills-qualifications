2bca5f0bc08a60d52582e7bf7e552a6b8f509bae
Mihir Lad <mihirlad55@gmail.com>
2020-03-03 00:04:18 -0500

    Add VexDb.py for fetching list of teams
    Currently only supports fetching list of teams by program or country.

---
+++ VexDb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from requests import get
+import json
+
+
+TEAMS_API_URL = "https://api.vexdb.io/v1/get_teams"
+
+class VexDb:
+    @classmethod
+    def get_teams(self, program, country):
+        params = {
+            'program': program,
+            'country': country
+        }
+
+        resp = get(TEAMS_API_URL, params=params)
+        resp_json = json.loads(resp.content)
+        teams = resp_json['result']
+
+        return teams
